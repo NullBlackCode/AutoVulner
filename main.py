f1c9a53fa9abe8ede60df3dea9916e22b0567eb8
BlackCode <amirevercode@gmail.com>
2026-06-28 03:51:57 -0400

    start main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+#BlackCode
+#Github: https://github.com/nullBlackCode
+
+#Graphical and Colors Modules 
+from PyQt5 import *
+
+#Netword Modules 
+import socket
+import requests
+
+#System Modules
+import time
+import os
+import sys
+import subprocess
+
+#---------------------------------
+
+#Main window
